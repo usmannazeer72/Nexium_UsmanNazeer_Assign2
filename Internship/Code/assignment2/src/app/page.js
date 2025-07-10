f664e09ac3693b6253011136ed077cf4a1acc860
Usman <usmannazeer72@gmail.com>
2025-07-10 21:55:43 +0500

    Feat: Redirection button added

--- Internship/Code/assignment2/src/app/page.js
+++ Internship/Code/assignment2/src/app/page.js
@@ -1,4 +1,8 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 bg-background text-foreground">
       <main className="flex flex-col items-center gap-6">
@@ -7,7 +11,10 @@ export default function Home() {
           Instantly get concise summaries of any blog post. Paste a blog link or
           text and let our AI do the rest!
         </p>
-        <button className="rounded-full bg-foreground text-background px-8 py-3 text-lg font-semibold shadow hover:bg-[#383838] dark:hover:bg-[#ccc] transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-foreground">
+        <button
+          className="rounded-full bg-foreground text-background px-8 py-3 text-lg font-semibold shadow hover:bg-[#383838] dark:hover:bg-[#ccc] transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-foreground"
+          onClick={() => router.push("/summariser")}
+        >
           Summarise a Blog
         </button>
       </main>
